c5e7ab680b50be16500089972a99b3345baeb997
SovyliaAdeleina <sovylia27@gmail.com>
2024-05-14 14:37:54 +0700

    JS 13 - Membuat Symlink & Method Move

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -50,10 +50,24 @@ class FileUploadController extends Controller
         $extfile=$request->berkas->getClientOriginalName();
         $namaFile='web-' .time().".".$extfile;
     //    $path = $request->berkas->storeAs('uploads', 'berkas');
-        $path = $request->berkas->storeAs('uploads', $namaFile);
-        echo "proses upload berhasil, data disimpan pada: $path ";
+
+        // Jobsheet 12 - E. Membuat Symlink
+    //    $path = $request->berkas->storeAs('public', $namaFile);
+        // $pathBaru=asset('storage/' .$namaFile);
+        // echo "proses upload berhasil, data disimpan pada: $path ";
+        // echo "<br>";
+        // echo "Tampilkan link:<a href='$pathBaru'>$pathBaru</a>";
     //   echo "proses upload berhasil, file berada di: $path ";
     //   echo $request->berkas->getClientOriginalName()."lolos validasi";
 
+        // Jobsheet 12 - F. Method Move
+        $path = $request->berkas->move('gambar', $namaFile);
+        $path =str_replace("\\","//", $path);
+        echo"Variabel path berisi: $path <br>";
+
+        $pathBaru=asset('gambar/' .$namaFile);
+        echo "proses upload berhasil, data disimpan pada: $path ";
+        echo "<br>";
+        echo "Tampilkan link:<a href='$pathBaru'>$pathBaru</a>";
     }
 }
